65018169d053997bc75039711b91e6d23fe212b8
Navidshahran <Navidshahran@tanjus-air.lehman.edu>
2020-02-24 20:05:13 -0500

    First Commit Uploading all the files

---
+++ Cunnect_4Game/src/Gui_Connect4/Connect_4_Player_Interface.java
@@ -0,0 +1,8 @@
+package Gui_Connect4;
+
+public interface Connect_4_Player_Interface {
+	public boolean isWinner();
+	public void takeTurn();
+
+
+}
---
+++ Cunnect_4Game/src/Gui_Connect4/Show_Connect4_Board.java
@@ -0,0 +1,8 @@
+package Gui_Connect4;
+
+public class Show_Connect4_Board {
+	public static void main (String[] args) {
+
+		new connect_4_GUI();
+	}
+}
